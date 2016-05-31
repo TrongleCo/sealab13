VOREStation/VOREStation

Adds a new vending machine that vends fitness related products

code/modules/reagents/Chemistry-Reagents/Chemistry-Reagents-Food-Drinks.dm
@@ -548,6 +548,16 @@
 	cup_name = "cup of milk"
 	cup_desc = "White and nutritious goodness!"
 
+/datum/reagent/drink/milk/chocolate
+	name =  "Chocolate Milk"
+	id = "chocolate_milk"
+	description = "A delicious mixture of perfectly healthy mix and terrible chocolate."
+	color = "#74533b"
+
+	glass_icon_state = "glass_brown"
+	cup_name = "cup of chocolate milk"
+	cup_desc = "Deliciously fattening!"
+
 /datum/reagent/drink/milk/affect_ingest(var/mob/living/carbon/M, var/alien, var/removed)
 	..()
 	if(alien == IS_DIONA)

TrongleCo/sealab13

Conflicts:
	code/game/machinery/vending.dm
	code/modules/reagents/Chemistry-Reagents/Chemistry-Reagents-Food-Drinks.dm
	code/modules/reagents/reagent_containers/pill.dm
	code/modules/reagents/reagent_containers/syringes.dm
Former-commit-id: f2dcb15469eff20001a8c394a2ae06ddafa30ad3

code/modules/reagents/Chemistry-Reagents/Chemistry-Reagents-Food-Drinks.dm
@@ -605,6 +605,17 @@
 	glass_name = "milk"
 	glass_desc = "White and nutritious goodness!"
 
+/datum/reagent/drink/milk/chocolate
+	name =  "Chocolate Milk"
+	id = "chocolate_milk"
+	description = "A delicious mixture of perfectly healthy mix and terrible chocolate."
+	taste_description = "chocolate milk"
+	color = "#74533b"
+
+	glass_icon_state = "glass_brown"
+	glass_name = "glass of chocolate milk"
+	glass_desc = "Deliciously fattening!"
+
 /datum/reagent/drink/milk/affect_ingest(var/mob/living/carbon/M, var/alien, var/removed)
 	..()
 	if(alien == IS_DIONA)